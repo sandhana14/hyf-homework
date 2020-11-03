f84bc192418eec818ccde5b36576f05d89327da2
sandhana14 <sandhana14@gmail.com>
2020-11-03 13:08:12 +0100

    Made some changes based on mentor's review

--- javascript/javascript1/week1/housepriceestimator.js
+++ javascript/javascript1/week1/housepriceestimator.js
@@ -1,6 +1,6 @@
 //House price estimator.
 let housePrice;
-let VolumeInMeters;
+let volumeInMeters;
 //House price estimation for Julia. 
 let gardenSizeInM2 = 70;
 let width = 5;
@@ -8,11 +8,14 @@ let height = 8;
 let depth = 11;
 let actualHousePrice = 1000000;
 
-VolumeInMeters = width*height*depth;
-housePrice = VolumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+volumeInMeters = width*height*depth;
+housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+
 
 if (actualHousePrice>housePrice){
     console.log("Julia is paying too much.");
+}else if(actualHousePrice === housePrice){
+    console.log("Julia is paying just right.");
 }
 else{
     console.log("Julia is paying too little.");
@@ -24,11 +27,13 @@ height = 10;
 depth = 10;
 actualHousePrice = 2500000;
 
-VolumeInMeters = width*height*depth;
-housePrice = VolumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+volumeInMeters = width*height*depth;
+housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 
 if (actualHousePrice>housePrice){
     console.log("Peter is paying too much.");
+}else if(actualHousePrice === housePrice){
+    console.log("Peter is paying just right.");
 }
 else{
     console.log("Peter is paying too little.");
--- javascript/javascript1/week1/startupnames.js
+++ javascript/javascript1/week1/startupnames.js
@@ -1,10 +1,12 @@
 //Startup name generator.
-let firstWords = ["Easy","Awesome","Corporate","Idea","Qualified","Omega","Smart","Expression","Sky","Wise"];
-let secondWords = ["Corporation","World","Enterprise","Arrow","Truth","Invest","Path","Consumer","Point","Frame"];
+let firstWord = ["Easy","Awesome","Corporate","Idea","Qualified","Omega","Smart","Expression","Sky","Wise"];
+let secondWord = ["Corporation","World","Enterprise","Arrow","Truth","Invest","Path","Consumer","Point","Frame"];
 let startupName;
-const randomNumber = Math.floor(Math.random() * 10) + 0;
+const randomNumberForFirstWord = Math.floor(Math.random() * 10) + 0;
+const randomNumberForSecondWord = Math.floor(Math.random() * 10) + 0;
 
 
-startupName  = firstWords[randomNumber] + " " + secondWords[randomNumber];
+startupName  = firstWord[randomNumberForFirstWord] + " " + secondWord[randomNumberForSecondWord];
 
 console.log('\"'+ "The startup: " + '\"' + startupName + '\"'  + " contains " + startupName.length + " characters." + '\"');
+
