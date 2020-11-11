6c0715268975920511ea1b5b1a500295b072774c
sandhana14 <sandhana14@gmail.com>
2020-11-11 21:43:21 +0100

    Changes made as per the request

--- javascript/javascript1/week2/scriptweek2.js
+++ javascript/javascript1/week2/scriptweek2.js
@@ -1,52 +1,46 @@
 //Flight booking fullname function
-
 function getFullname(firstname,lastname,useFormalName){
     if(firstname === "" || lastname === ""){
         return "Please enter the first name and last name,It cannot be null/empty.";
     }
-    if(firstname !== "" || lastname !== ""){
     if(useFormalName){
-        let formalName = "Lord";
+        const formalName = "Lord";
         return "The full name is: " + formalName + " " + firstname + " " +lastname;
     }else{
         return "The full name is: " + firstname + " " +lastname;
 }
 }
-}
-
-let fullname1 = getFullname("","");
-let fullname2 = getFullname("Murali","Rajendran",true);
-let fullname3 = getFullname("Rithvik","Murali",false);
-let fullname4 = getFullname("sandhana","Ravi");
+const fullname1 = getFullname("","");
+const fullname2 = getFullname("Murali","Rajendran",true);
+const fullname3 = getFullname("Rithvik","Murali",false);
+const fullname4 = getFullname("sandhana","Ravi");
 console.log(fullname1);
 console.log(fullname2);
 console.log(fullname3);
 console.log(fullname4);
 
-//Event application
 
-let day = new Date();
-let weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-let today = weekday[day.getDay()];
-let eventWillBeHeldInDays = 2;
+//Event application
+const day = new Date();
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const today = weekday[day.getDay()];
+const eventWillBeHeldInDays = 2;
 
 function getEventWeekday(eventDay){
-    let result = ((day.getDay() + eventDay)+7)%7;
+    const result = ((day.getDay() + eventDay)+7)%7;
     return weekday[result];
 }
-
 console.log("Today is " + today + " and the event is in " + eventWillBeHeldInDays + " days." + "Therefore the event will be held on a " + getEventWeekday(eventWillBeHeldInDays) + ".");
 
 //Weather wear
-
-let temperature = 30
+const temperature = 30
 
 function wearClothesBasedOnTemperature(temp){
-    if(temp>20){
+    if(temp>=20){
         return "Shorts and T-Shirts"; 
-    }else if(temp>10){
+    }else if(temp>=10){
         return "Sweaters and Jeans";
-    }else if(temp>1){
+    }else if(temp>=1){
         return "Winter Jackets and Coats";
     }else{
         return "Scarf,Warm glooves and a knit headband and winter jackets";
@@ -55,19 +49,16 @@ const clothesToWear = wearClothesBasedOnTemperature(temperature);
 console.log("You should wear " + clothesToWear + " during " + temperature + " degree celsius temperature.");
 
 // Student manager
-
 const class07Students = [];
 function addStudentToClass(studentName) {
-    if(studentName === "Queen Margrethe II"){
-        if(class07Students.length < 6 || class07Students.length>=6){
-            class07Students.push(studentName);
-        }
-    }else if (class07Students.length>=6){
-        console.log("Cannot add more students to class 07.");
+    if(studentName === " "){
+        console.log("You cannot add an empty string to a class.");
     }else if(class07Students.includes(studentName)){
         console.log("Student " + studentName + " is already in the class.");
-    }else if(studentName === " "){
-        console.log("You cannot add an empty string to a class.");
+    }else if(studentName === "Queen Margrethe II"){
+        class07Students.push(studentName);
+    }else if (class07Students.length>=6){
+        console.log("Cannot add more students to class 07.");
     }else{
         class07Students.push(studentName);
         return class07Students;
@@ -77,41 +68,44 @@ function getNumberOfStudents() {
     return class07Students.length;
 }
 addStudentToClass("Sandhana");
-addStudentToClass("Murali");
-addStudentToClass("Sandy");
+addStudentToClass("Saravanan");
 addStudentToClass("Rithvik");
 addStudentToClass("Ravi");
 addStudentToClass("Selvi");
+addStudentToClass("Arun");
 addStudentToClass("Queen Margrethe II");
-
 console.log(class07Students);
 console.log("The number of students in the class is "+ getNumberOfStudents());
 
 //Candy helper
 
-let boughtCandyPrices = [];
+const boughtCandyPrices = [];
 let totalPrice = 0;
-let amountToSpend = Math.round(Math.random() * 100) ;
+const amountToSpend = Math.round(Math.random() * 100) ;
 
 function addCandy(candyType,weight){
-    if(candyType === "sweet"){
-        let price = weight*0.5;
+    let price;
+    switch(candyType){
+    case "sweet":
+        price = weight*0.5;
         boughtCandyPrices.push(price);
-    }else if(candyType === "Chocolate"){
+        break;
+    case "Chocolate":
         price = weight*0.7;
         boughtCandyPrices.push(price);
-    }else if(candyType === "Toffee"){
+        break;
+    case "Toffee":
         price = weight*1.1;
         boughtCandyPrices.push(price);
-    }else if(candyType === "Chewing-gum"){
+        break;
+    case "Chewing-gum":
         price = weight*0.03;
         boughtCandyPrices.push(price);
+        break;
     }
 }
-
 addCandy("sweet",20);
 addCandy("Chocolate",50);
-
 for(i=0; i<boughtCandyPrices.length; i++){
     totalPrice += boughtCandyPrices[i];
 }
@@ -122,8 +116,7 @@ function canBuyMoreCandy(){
         return false;
     }
 }
-
-if(canBuyMoreCandy() === true){
+if(canBuyMoreCandy()){
         console.log("The amount to spend is " + amountToSpend + " and " + "The amount you spent is " + totalPrice + " So, You can buy more, so please do!");
     }else{
         console.log("The amount to spend is " + amountToSpend + " and " + "The amount you spent is " + totalPrice + " So,enough candy for you!");
