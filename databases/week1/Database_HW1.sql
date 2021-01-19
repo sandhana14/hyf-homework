b45ed89a64b9887858f2512d0f0773758b96fc45
sandhana14 <sandhana14@gmail.com>
2021-01-19 16:18:47 +0100

    Changed as per the request

--- databases/week1/Database_HW1.sql
+++ databases/week1/Database_HW1.sql
@@ -6,13 +6,13 @@ SELECT COUNT(*) FROM task
 WHERE due_date IS NULL;
 
 /*3.Find all the tasks that are marked as done*/
-SELECT task.id,task.title,task.description,task.created,task.updated,task.due_date,task.status_id,task.user_id 
-FROM task JOIN status ON task.status_id = status.id
+SELECT t.id,t.title,t.description,t.created,t.updated,t.due_date,t.status_id,t.user_id 
+FROM task t JOIN status ON t.status_id = status.id
 WHERE status.name = "Done";
 
 /*4.Find all the tasks that are not marked as done*/
-SELECT task.id,task.title,task.description,task.created,task.updated,task.due_date,task.status_id,task.user_id 
-FROM task JOIN status ON task.status_id = status.id
+SELECT t.id,t.title,t.description,t.created,t.updated,t.due_date,t.status_id,t.user_id 
+FROM task t JOIN status ON t.status_id = status.id
 WHERE status.name = "In progress" OR status.name = "Not started";
 
 /*5.Get all the tasks, sorted with the most recently created first*/
@@ -28,18 +28,20 @@ SELECT title,due_date FROM task
 WHERE title LIKE "%da%se%" OR description LIKE "%da%se%";
 
 /*8.Get the title and status (as text) of all tasks*/
-SELECT title,status_id AS text FROM task;
 SELECT task.title,status.name AS text
-FROM task JOIN status ON task.status_id = status.id;
+FROM task INNER JOIN status ON task.status_id = status.id;
 
 /*9.Get the name of each status, along with a count of how many tasks have that status*/
 SELECT status.name, count(*)
 FROM status JOIN task ON task.status_id = status.id
 GROUP BY status.name;
+SELECT * FROM TASK;
+SELECT * FROM STATUS;
 
 /*10.Get the names of all statuses, sorted by the status with most tasks first*/
 SELECT status.name, count(*)
 FROM status JOIN task ON task.status_id = status.id
 GROUP BY status.name
 ORDER BY count(*) DESC;
+
 
