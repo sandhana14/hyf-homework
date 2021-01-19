6733d87f47b000730016718dd42c6ab042ea394d
sandhana14 <sandhana14@gmail.com>
2021-01-19 22:04:09 +0100

    Changed as per the request

--- javascript/javascript3/week2/mainVisualjs.js
+++ javascript/javascript3/week2/mainVisualjs.js
@@ -1,29 +1,54 @@
-async function translateOneByOne() {
-    try {
-        await moveElement(document.querySelector('ul.marks li:nth-child(1)'), { x: (20 - parseInt(document.querySelector('ul.marks li:nth-child(1)').style.left)), y: (300 - parseInt(document.querySelector('ul.marks li:nth-child(1)').style.top)) })
-        console.log("Red target element has been moved");
+const redBox = document.querySelector("ul.marks li:nth-child(1)");
+const blueBox = document.querySelector("ul.marks li:nth-child(2)");
+const greenBox = document.querySelector("ul.marks li:nth-child(3)");
 
-        await moveElement(document.querySelector('ul.marks li:nth-child(2)'), { x: (400 - parseInt(document.querySelector('ul.marks li:nth-child(2)').style.left)), y: (300 - parseInt(document.querySelector('ul.marks li:nth-child(2)').style.top)) })
-        console.log("Blue target element has been moved");
-
-        await moveElement(document.querySelector('ul.marks li:nth-child(3)'), { x: (400 - parseInt(document.querySelector('ul.marks li:nth-child(3)').style.left)), y: (20 - parseInt(document.querySelector('ul.marks li:nth-child(3)').style.top)) })
-        console.log("Green target element has been moved");
-    } catch (err) {
-        console.error(err);
-    }
+function calculateTargetPosition(targetPosition, boxToMove) {
+  const boxToMoveTargetLeft = targetPosition.x - parseInt(boxToMove.style.left);
+  const boxToMoveTargetTop = targetPosition.y - parseInt(boxToMove.style.top);
+  return { x: boxToMoveTargetLeft, y: boxToMoveTargetTop };
 }
 
+async function translateOneByOne() {
+  try {
+    await moveElement(
+      redBox,
+      calculateTargetPosition({ x: 20, y: 300 }, redBox)
+    );
+    console.log("Red target element has been moved");
+
+    await moveElement(
+      blueBox,
+      calculateTargetPosition({ x: 400, y: 300 }, blueBox)
+    );
+    console.log("Blue target element has been moved");
+
+    await moveElement(
+      greenBox,
+      calculateTargetPosition({ x: 400, y: 20 }, greenBox)
+    );
+    console.log("Green target element has been moved");
+  } catch (err) {
+    console.error(err);
+  }
+}
 translateOneByOne();
 
-
 async function translateAllAtOnce() {
-    try {
-        await Promise.all([moveElement(document.querySelector('ul.marks li:nth-child(1)'), { x: (20 - parseInt(document.querySelector('ul.marks li:nth-child(1)').style.left)), y: (300 - parseInt(document.querySelector('ul.marks li:nth-child(1)').style.top)) }), moveElement(document.querySelector('ul.marks li:nth-child(2)'), { x: (400 - parseInt(document.querySelector('ul.marks li:nth-child(2)').style.left)), y: (300 - parseInt(document.querySelector('ul.marks li:nth-child(2)').style.top)) }), moveElement(document.querySelector('ul.marks li:nth-child(3)'), { x: (400 - parseInt(document.querySelector('ul.marks li:nth-child(3)').style.left)), y: (20 - parseInt(document.querySelector('ul.marks li:nth-child(3)').style.top)) })]);
-        console.log("All three color element have been moved");
-    } catch (err) {
-        console.error(err);
-    }
-
+  try {
+    await Promise.all([
+      moveElement(redBox, calculateTargetPosition({ x: 20, y: 300 }, redBox)),
+      moveElement(
+        blueBox,
+        calculateTargetPosition({ x: 400, y: 300 }, blueBox)
+      ),
+      moveElement(
+        greenBox,
+        calculateTargetPosition({ x: 400, y: 20 }, greenBox)
+      ),
+    ]);
+    console.log("All three color element have been moved");
+  } catch (err) {
+    console.error(err);
+  }
 }
 translateAllAtOnce();
-
