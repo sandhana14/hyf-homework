9b853d94358d9e8c4fb561ec705c29c77e432c93
sandhana14 <sandhana14@gmail.com>
2021-03-09 12:31:06 +0100

    Added React week3 homework files

---
+++ react/week3/todo-list/src/AddTodo.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+
+function Addtodo({ addTodosAfterButtonClick }) {
+  const [description, setDescription] = useState("");
+  const [deadline, setDeadline] = useState("");
+  const [validDate, setValidDate] = useState(true);
+
+  const onChangeInputDescriptionValue = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const currentdate = () => {
+    const today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) dd = "0" + dd;
+    if (mm < 10) mm = "0" + mm;
+    return yyyy + "-" + mm + "-" + dd;
+  };
+
+  const error = () => {
+    return "Enter the current date or above in the deadline field!!!!!";
+  };
+
+  const onChangeInputDeadlineValue = (event) => {
+    if (event.target.value >= currentdate()) {
+      setDeadline(event.target.value);
+      setValidDate(true);
+    } else {
+      setDeadline("");
+      setValidDate(!validDate);
+    }
+  };
+
+  return (
+    <>
+      <div className="input-group">
+        <label htmlFor="description">Todo Description</label>
+        <input
+          className="input"
+          onChange={onChangeInputDescriptionValue}
+          type="text"
+          value={description}
+          placeholder="Enter the todo item here"
+          id="description"
+        ></input>
+      </div>
+
+      <div className="input-group">
+        <label htmlFor="deadline">Deadline</label>
+        <input
+          onChange={onChangeInputDeadlineValue}
+          type="date"
+          value={deadline}
+          placeholder="Enter the deadline here"
+          id="deadline"
+          className="input"
+        ></input>
+      </div>
+
+      {!validDate && <h3 className="error-tag">{error()}</h3>}
+
+      <div>
+        <button
+          onClick={() => {
+            addTodosAfterButtonClick(description, deadline);
+            setDescription("");
+            setDeadline("");
+          }}
+          className="add-button"
+        >
+          Add Todo item
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Addtodo;
